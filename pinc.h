563e0999d35f4ba0de648d36d2a9f07a6cd5a8f7
Lakindu Akash <Lakinduakash@gmail.com>
2017-12-12 23:48:27 +0530

    Add files via upload

---
+++ pinc.h
@@ -0,0 +1,68 @@
+
+#ifndef PINC_HEADER // #include guarde
+#define PINC_HEADER 5
+
+#define INPUT 1 //define input bit
+#define OUTPUT 0 //define output bit
+#define HIGH 1 // define logic high
+#define LOW 0 // define logic low
+
+/*
+ * set specific pin in a port as a output or input.
+ * It should be passed address of TRIS register as parameter for <code>volatile unsigned char *tris</code>
+ * <code>volatile unsigned char pin</code> is pin number of specific port 0 to 7
+ * Last parameter is passed as type INPUT or OUTPUT
+ */
+
+void pinMode(volatile unsigned char *tris,unsigned char pin,unsigned char io)
+{
+    
+    if(io==1)
+    {
+        unsigned char v=0b00000001;
+        v<<=pin;
+        *tris |=v;
+    }
+    else
+    {
+        unsigned char v=0b11111110;
+        unsigned char v1=0b00000001;
+        v1<<=pin;
+        v1=(unsigned char)~v1;
+        v<<=pin;
+        v|=v1;
+        *tris & = v;
+    }
+}
+
+void setPin(volatile unsigned char *port,unsigned char pin,unsigned char value )
+{
+    if(value==1)
+    {
+        unsigned char v=0b00000001;
+        v<<=pin;
+        *port |=v;
+    }
+    else
+    {
+        unsigned char v=0b11111110;
+        unsigned char v1=0b00000001;
+        v1<<=pin;
+        v1=(unsigned char)~v1;
+        v<<=pin;
+        v|=v1;
+        *port & = v;
+    }
+}
+
+unsigned char readPin(volatile unsigned char *port,unsigned char pin)
+{
+    unsigned char v=0b00000001;
+    
+    if(((v<<pin)&(*port))>0)
+        return 1;
+    else
+        return 0;
+}
+
+#endif //PINC_HEADER
